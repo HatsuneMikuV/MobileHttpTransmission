a4846d8ff20b06e1a9b24b4e6d68ef4d71bc8c0b
joe.liu <joe.liu@cootek.cn>
2020-07-30 19:04:45 +0800

    [update] a -->b b-->a

--- MobileHttpDemo/MobileHttpDemo/ViewController.swift
+++ MobileHttpDemo/MobileHttpDemo/ViewController.swift
@@ -38,32 +38,64 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     SVProgressHUD.setMaximumDismissTimeInterval(2)
+    AF.sessionConfiguration.timeoutIntervalForRequest = 15
+    AF.sessionConfiguration.timeoutIntervalForRequest = 15
   }
   
   @IBAction func createSever(_ sender: Any) {
+    self.createBtn.isSelected = !self.createBtn.isSelected
     view.endEditing(true)
-    if self.webServer == nil {
-      let webServer = GCDWebServer()
-      webServer.delegate = self
-      webServer.addDefaultHandler(forMethod: "GET", request: GCDWebServerRequest.self, asyncProcessBlock: { (request, completionBlock) in
-        
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1, execute: {
+    if self.createBtn.isSelected {
+      self.createBtn.setTitle("关闭服务器", for: .normal)
+      if self.webServer == nil {
+        let webServer = GCDWebServer()
+        webServer.delegate = self
+        webServer.addDefaultHandler(forMethod: "GET", request: GCDWebServerRequest.self, asyncProcessBlock: { (request, completionBlock) in
           
-          let response = GCDWebServerDataResponse(html: self.contentTxt.text ?? "Test data")
-          completionBlock(response)
-          
+          /// do something
+          DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1, execute: {
+            var content = "Test data"
+            if let str = self.contentTxt.text, str.count > 0 {
+              content = str
+            }
+            let response = GCDWebServerDataResponse.init(text: content)
+            completionBlock(response)
+          })
         })
-      })
-      
-      webServer.start(withPort: 8080, bonjourName: "GCD Web Server")
-      print("The service started successfully: \(webServer.serverURL!)")
-      self.httpIPLabel.text = "ip or http: " + webServer.serverURL!.absoluteString
-      self.webServer = webServer
-      self.sendBtn.isHidden = true
-      self.tipsLabel.isHidden = true
-      self.resultLabel.isHidden = true
+        
+        webServer.start(withPort: 8080, bonjourName: "GCD Web Server")
+        print("The service started successfully: \(webServer.serverURL)")
+        if webServer.serverURL == nil {
+          webServer.stop()
+          self.createBtn.isSelected = false
+          self.createBtn.setTitle("创建服务器", for: .normal)
+          SVProgressHUD.showError(withStatus: "Please check mobile wifi!!!")
+        } else {
+          self.httpIPLabel.text = "ip or http: " + String(webServer.serverURL?.absoluteString ?? "")
+          self.webServer = webServer
+          self.sendBtn.isHidden = true
+          self.tipsLabel.isHidden = true
+          self.resultLabel.isHidden = true
+        }
+        self.httpIPLabel.isHidden = false
+        self.chooseBtn.isHidden = false
+      } else {
+        SVProgressHUD.showError(withStatus: "Created!!!")
+      }
     } else {
-      SVProgressHUD.showError(withStatus: "Created!!!")
+      self.createBtn.setTitle("创建服务器", for: .normal)
+      if self.webServer == nil {
+        SVProgressHUD.showError(withStatus: "Closed!!!")
+      } else {
+        self.webServer?.stop()
+        self.webServer = nil
+        self.httpIPLabel.text = "ip or http: ???"
+        self.sendBtn.isHidden = false
+        self.tipsLabel.isHidden = false
+        self.resultLabel.isHidden = false
+        self.httpIPLabel.isHidden = false
+        self.chooseBtn.isHidden = false
+      }
     }
   }
   
@@ -71,7 +103,6 @@ class ViewController: UIViewController {
     view.endEditing(true)
     self.httpIPLabel.isHidden = true
     self.chooseBtn.isHidden = true
-    self.createBtn.isHidden = true
     if self.contentTxt.text?.count ?? 0 > 0 {
       AF.request(self.contentTxt.text!).responseString(completionHandler: { (response) in
         switch response.result {
@@ -101,28 +132,28 @@ class ViewController: UIViewController {
 
 
 extension ViewController:GCDWebServerDelegate {
-  func webServerDidStop(_ server: GCDWebServer) {
-    print("webServerDidStop===========")
-    print("1======\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
-  }
+  
   func webServerDidStart(_ server: GCDWebServer) {
-    print("webServerDidStart===========")
-    print("2======\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
+    print("webServerDidStart===========\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
+  }
+  
+  func webServerDidCompleteBonjourRegistration(_ server: GCDWebServer) {
+    print("webServerDidCompleteBonjourRegistration===========\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
   }
   func webServerDidConnect(_ server: GCDWebServer) {
-    print("webServerDidConnect===========")
-    print("3======\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
+    print("webServerDidConnect===========\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
   }
+  
   func webServerDidDisconnect(_ server: GCDWebServer) {
-    print("webServerDidDisconnect===========")
-    print("4======\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
+    print("webServerDidDisconnect===========\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
   }
+  
+  func webServerDidStop(_ server: GCDWebServer) {
+    print("webServerDidStop===========\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
+  }
+  
   func webServerDidUpdateNATPortMapping(_ server: GCDWebServer) {
-    print("webServerDidUpdateNATPortMapping===========")
-    print("5======\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
+    print("webServerDidUpdateNATPortMapping===========\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
   }
-  func webServerDidCompleteBonjourRegistration(_ server: GCDWebServer) {
-    print("webServerDidCompleteBonjourRegistration===========")
-    print("6======\nport: \(server.port) \nbonjourName: \(server.bonjourName)\n bonjourServerURL: \(server.bonjourServerURL) \nbonjourType: \(server.bonjourType)\npublicServerURL: \(server.publicServerURL)\nserverURL: \(server.serverURL)")
-  }
+  
 }
